0104e333cf3e1ea1a45963fd3fa67d51bd910413
alexeys6969 <alexshashin.2023@gmail.com>
2025-12-15 16:56:49 +0500

    Разработанные методы отправки сообщений и команд. mamaev

--- Worker.cs
+++ Worker.cs
@@ -1,5 +1,6 @@
 using pr9.Classes;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace pr9
@@ -47,7 +48,7 @@ namespace pr9
             {
                 Keyboard = new List<List<KeyboardButton>>
                 {
-                    keyboardButtons 
+                    keyboardButtons
                 }
             };
         }
@@ -57,5 +58,47 @@ namespace pr9
             inlineKeyboards.Add(new InlineKeyboardButton("Удалить", Message));
             return new InlineKeyboardMarkup(inlineKeyboards);
         }
-    } 
+        public async void SendMessage(long chatId, int typeMessage)
+        {
+            if (typeMessage != 3)
+            {
+                await TelegramBotClient.SendMessage
+                    (
+                        chatId,
+                        Messages[typeMessage],
+                        ParseMode.Html,
+                        replyMarkup: GetButtons()
+                    );
+            }
+            else if (typeMessage == 3)
+                await TelegramBotClient.SendMessage(
+                    chatId,
+                    $"Указанное вами время и дата не могут быть установлены, " +
+                    $"потму что сейчас уже:{DateTime.Now.ToString("HH.mm dd.MM.yyyy")}");
+        }
+        public async void Command(long chatId, string command)
+        {
+            if (command.ToLower() == "/start") SendMessage(chatId, 0);
+            else if (command.ToLower() == "/create_tasks") SendMessage(chatId, 1);
+            else if (command.ToLower() == "/list_tasks")
+            {
+                Users User = Users.Find(x => x.IdUser == chatId);
+                if (User == null) SendMessage(chatId, 4);
+                else if (User.Events.Count == 0) SendMessage(chatId, 4);
+                else
+                {
+                    foreach (Events Event in User.Events)
+                    {
+                        await TelegramBotClient.SendMessage
+                            (
+                            chatId,
+                            $"Уведомить пользователя: {Event.Time.ToString("HH:mm dd:MM:yyyy")}"
+                            + $"\nСообщение: {Event.Message}",
+                            replyMarkup: DeleteEvent(Event.Message)
+                            );
+                    }
+                }
+            }
+        }
+    }
 }
