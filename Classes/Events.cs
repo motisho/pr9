6c658cc4b43f76ee0f23d21f008f982c6af5400a
alexeys6969 <alexshashin.2023@gmail.com>
2025-12-15 16:31:16 +0500

    Добавьте файлы проекта.

---
+++ Classes/Events.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr9.Classes
+{
+    public class Events
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+
+        public Events(DateTime time, string message) 
+        {
+            Time = time;
+            Message = message;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+using pr9;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
